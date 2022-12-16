7bb61011b9b1b6011afb11d7e65e179884bb2e15
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-12-16 11:32:41 -0500

    Creación de la entidad FiscalPosition

---
+++ src/main/java/sg/gov/govsgbackend/entity/FiscalPosition.java
@@ -0,0 +1,49 @@
+package sg.gov.govsgbackend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class FiscalPosition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "year_of_balance")
+    private Integer yearOfBalance;
+
+    @NotBlank
+    @NotNull
+    @Column(name = "actual_revised_estimated")
+    private String actualRevisedEstimated;
+
+    @NotBlank(message = "La categoría no puede estar vacía")
+    @NotNull
+    @Column(name = "category")
+    private String category;
+
+    @NotBlank(message = "El item no puede estar vacío")
+    @NotNull
+    @Column(name = "item")
+    private String item;
+
+    @NotNull
+    @Column(name = "amount")
+    private Integer amount;
+
+    @NotNull
+    @Column(name = "percent_of_gdp")
+    private BigDecimal percentOfGdp;
+
+}
