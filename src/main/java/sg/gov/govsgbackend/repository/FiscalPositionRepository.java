47857b08bf57df600d8eacab0a2eb18870a3110e
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-12-16 12:19:20 -0500

    Creación del Repository de FiscalPosition

---
+++ src/main/java/sg/gov/govsgbackend/repository/FiscalPositionRepository.java
@@ -0,0 +1,10 @@
+package sg.gov.govsgbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sg.gov.govsgbackend.entity.FiscalPosition;
+
+public interface FiscalPositionRepository extends JpaRepository<FiscalPosition, Long> {
+
+
+
+}
