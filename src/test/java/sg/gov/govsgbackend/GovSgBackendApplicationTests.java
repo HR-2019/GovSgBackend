03b04a015c5814b7e9ac4210eaf557be49b415c3
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-12-16 08:56:37 -0500

    Creación del proyecto

---
+++ src/test/java/sg/gov/govsgbackend/GovSgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.gov.govsgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovSgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
