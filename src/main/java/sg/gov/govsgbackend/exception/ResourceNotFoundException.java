d84aa2cd431e1ab3dc39357b6f30884adbb4c853
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-12-17 10:21:42 -0500

    Creación de la excepción para Not Found

---
+++ src/main/java/sg/gov/govsgbackend/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package sg.gov.govsgbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {
+        super();
+    }
+
+}
